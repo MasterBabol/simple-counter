21cd295ede6073a35fbc22abb1d4e108a280c30e
Kibin Park <babol@live.co.kr>
2022-02-25 21:14:57 +0900

    Add wstrb supp.

--- simple_counter.v
+++ simple_counter.v
@@ -3,7 +3,8 @@
 module simple_counter
 #
 (
-    parameter integer axi_addr_width    = 32
+    parameter integer axi_addr_width    = 32,
+    parameter integer p_axi_data_width  = 128
 )
 (
     clk                 ,
@@ -26,42 +27,46 @@ module simple_counter
     s_axi_ctl_rvalid    ,
     s_axi_ctl_rready    ,
     s_axi_ctl_rresp     ,
-    probe
+    p_axi_wvalid        ,
+    p_axi_wready        ,
+    p_axi_wstrb
 );
-    input                           clk                 ;
-    input                           srstn               ;
-    input                           enable              ;
+    input                               clk                 ;
+    input                               srstn               ;
+    input                               enable              ;
 
-    input   [axi_addr_width - 1:0]  s_axi_ctl_awaddr    ;
-    input                           s_axi_ctl_awvalid   ;
-    output                          s_axi_ctl_awready   ;
-    input   [32 - 1:0]              s_axi_ctl_wdata     ;
-    input   [32/8 - 1:0]            s_axi_ctl_wstrb     ;
-    input                           s_axi_ctl_wvalid    ;
-    output                          s_axi_ctl_wready    ;
-    output  [1:0]                   s_axi_ctl_bresp     ;
-    output                          s_axi_ctl_bvalid    ;
-    input                           s_axi_ctl_bready    ;
+    input   [axi_addr_width - 1:0]      s_axi_ctl_awaddr    ;
+    input                               s_axi_ctl_awvalid   ;
+    output                              s_axi_ctl_awready   ;
+    input   [32 - 1:0]                  s_axi_ctl_wdata     ;
+    input   [32/8 - 1:0]                s_axi_ctl_wstrb     ;
+    input                               s_axi_ctl_wvalid    ;
+    output                              s_axi_ctl_wready    ;
+    output  [1:0]                       s_axi_ctl_bresp     ;
+    output                              s_axi_ctl_bvalid    ;
+    input                               s_axi_ctl_bready    ;
 
-    input   [axi_addr_width - 1:0]  s_axi_ctl_araddr    ;
-    input                           s_axi_ctl_arvalid   ;
-    output                          s_axi_ctl_arready   ;
-    output  [32 - 1:0]              s_axi_ctl_rdata     ;
-    output                          s_axi_ctl_rvalid    ;
-    input                           s_axi_ctl_rready    ;
-    output  [1:0]                   s_axi_ctl_rresp     ;
+    input   [axi_addr_width - 1:0]      s_axi_ctl_araddr    ;
+    input                               s_axi_ctl_arvalid   ;
+    output                              s_axi_ctl_arready   ;
+    output  [32 - 1:0]                  s_axi_ctl_rdata     ;
+    output                              s_axi_ctl_rvalid    ;
+    input                               s_axi_ctl_rready    ;
+    output  [1:0]                       s_axi_ctl_rresp     ;
 
-    input                           probe               ;
+    input                               p_axi_wvalid        ;
+    input                               p_axi_wready        ;
+    input   [p_axi_data_width/8 - 1:0]  p_axi_wstrb         ;
 
-    wire    [axi_addr_width - 1:0]  w_ctl_waddr         ;
-    wire    [32 - 1:0]              w_ctl_wdata         ;
-    wire    [32/8 - 1:0]            w_ctl_wstrb         ;
-    wire                            w_ctl_wvalid        ;
-    wire                            w_ctl_wready        ;
-    wire    [axi_addr_width - 1:0]  w_ctl_raddr         ;
-    reg     [32 - 1:0]              r_ctl_rdata         ;
-    wire                            w_ctl_rvalid        ;
-    wire                            w_ctl_rready        ;
+    wire    [axi_addr_width - 1:0]      w_ctl_waddr         ;
+    wire    [32 - 1:0]                  w_ctl_wdata         ;
+    wire    [32/8 - 1:0]                w_ctl_wstrb         ;
+    wire                                w_ctl_wvalid        ;
+    wire                                w_ctl_wready        ;
+    wire    [axi_addr_width - 1:0]      w_ctl_raddr         ;
+    reg     [32 - 1:0]                  r_ctl_rdata         ;
+    wire                                w_ctl_rvalid        ;
+    wire                                w_ctl_rready        ;
 
     axi4l_sif
     #
@@ -103,17 +108,31 @@ module simple_counter
         .acc__rready        (w_ctl_rready       )
     );
 
+    integer i;
+    reg [$clog2(p_axi_data_width/8):0] r_wdata_bytes;
+
     reg [31:0] r_counter;
 
     assign w_ctl_wready = 1'b1;
     assign w_ctl_rready = 1'b1;
     assign r_ctl_rdata = r_counter;
 
+    generate
+        always @ (*)
+        begin
+            r_wdata_bytes = 'b0;
+            for (i = 0; i < p_axi_data_width/8; i = i + 1)
+            begin: w_wdata_bytes
+                r_wdata_bytes = r_wdata_bytes + p_axi_wstrb[i];
+            end
+        end
+    endgenerate
+
     always @ (posedge clk)
         if (!srstn || !enable)
             r_counter <= 'b0;
         else
-            if (probe)
-                r_counter <= r_counter + 1;
+            if (p_axi_wvalid && p_axi_wready)
+                r_counter <= r_counter + r_wdata_bytes;
 
 endmodule
